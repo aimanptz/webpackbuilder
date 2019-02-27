07c684dd0e46b62ce1e8316be626b8b602ca3b3c
Pavel Kuznetsov <aimanptz@gmail.com>
2019-02-27 13:30:13 +0300

    Update webpack.dev.conf.js

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,26 +1,38 @@
-const webpack = require('webpack'),
-  merge = require('webpack-merge'),
-  baseWebpackConfig = require('./webpack.base.config');
+const webpack           = require('webpack'),
+			path              = require('path'),
+			merge             = require('webpack-merge'),
+			baseWebpackConfig = require('./webpack.base.config');
+
+const PATHS = {
+	src: path.join(__dirname, '../src'),
+	dist: path.join(__dirname, '../dist'),
+	assets: 'assets/'
+};
 
 const devWebpackConfig = merge(baseWebpackConfig, {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: baseWebpackConfig.externals.paths.dist,
-    port: 8081,
-    /*overlay: {
-      warnings: false,
-      errors: true
-    }*/
-  },
-  plugins: [
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map'
-    })
-  ]
+	mode: 'development',
+	output: {
+		filename: `js/[name].js`,
+		path: PATHS.dist,
+		publicPath: '/'
+	},
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: baseWebpackConfig.externals.paths.dist,
+		port: 8081,
+		/*overlay: {
+			warnings: false,
+			errors: true
+		}*/
+	},
+	plugins: [
+		new webpack.SourceMapDevToolPlugin({
+			filename: '[file].map'
+		})
+	]
 });
 
-module.exports = new Promise((resolve,reject) => {
-  resolve(devWebpackConfig);
+module.exports = new Promise((resolve, reject) => {
+	resolve(devWebpackConfig);
 });
 
