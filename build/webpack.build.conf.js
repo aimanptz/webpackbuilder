c5c1530ccc831c98c9fa6726cb9cd3472523cda4
Pavel Kuznetsov <aimanptz@gmail.com>
2019-02-27 13:29:58 +0300

    Update webpack.build.conf.js

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,21 +1,32 @@
-const merge = require('webpack-merge'),
-  baseWebpackConfig = require('./webpack.base.config'),
-  ImageminPlugin = require('imagemin-webpack-plugin').default,
-  imageminMozjpeg = require('imagemin-mozjpeg');
+const merge             = require('webpack-merge'),
+			path              = require('path'),
+			baseWebpackConfig = require('./webpack.base.config'),
+			ImageminPlugin    = require('imagemin-webpack-plugin').default,
+			imageminMozjpeg   = require('imagemin-mozjpeg');
 
+const PATHS = {
+	src: path.join(__dirname, '../src'),
+	dist: path.join(__dirname, '../dist'),
+	assets: 'assets/'
+};
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
-    mode: 'production',
-    plugins: [
-      new ImageminPlugin({
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        pngquant: {quality: '50'},
-        svgo: true,
-        plugins: [imageminMozjpeg({quality: '50'})]
-      })
-    ]
-  });
+	mode: 'production',
+	output: {
+		filename: `js/[name].js`,
+		path: PATHS.dist,
+		publicPath: './'
+	},
+	plugins: [
+		new ImageminPlugin({
+			test: /\.(jpe?g|png|gif|svg)$/i,
+			pngquant: {quality: '50'},
+			svgo: true,
+			plugins: [imageminMozjpeg({quality: '50'})]
+		})
+	]
+});
 
-module.exports = new Promise((resolve,reject) => {
-  resolve(buildWebpackConfig);
+module.exports = new Promise((resolve, reject) => {
+	resolve(buildWebpackConfig);
 });
